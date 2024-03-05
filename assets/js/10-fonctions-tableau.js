92dbaadd5707baef0063970862b306ecf22b19fa
knz <kenza.mroudjae@outlook.com>
2024-03-05 17:01:29 +0100

    cours java

--- assets/js/10-fonctions-tableau.js
+++ assets/js/10-fonctions-tableau.js
@@ -19,3 +19,31 @@ console.log(ajout)
 // Supprimer le premier élement du tableau 
 const delet =animal.shift()
 console.log(delet)
+
+// Reorganiser le tableau de la fin vers le début 
+animal.reverse()
+console.log(animal)
+
+// Modifier ou ajouter ou supprimer un élement dans un tableau 
+// Ajouter un raton laveur en deuxiéme position
+animal.splice(1,0, "raton laveur")
+// Modifier chien en aigle
+animal.splice(5,1, "aigle")
+console.log(animal)
+// Supprimer
+animal.splice(2, 1)
+console.log(animal)
+// Déclare un tableau contenant (Paris, Berlin,Rome, Moscou, Londre, Madrid)
+const city= ["Paris" ,"Berlin","Rome"," Moscou,","Londres"," Madrid"]
+console.log(city)
+/* Créer une fonction qui prend en paramétre un tableau:
+-Rtire Madrird
+-Ajoute au debbut Vienne
+-Rome devient alger
+-Retourne la taille du tableau */
+function ville(){
+    city.pop()
+    city.unshift("Vienne")
+
+}
+ville(city)
